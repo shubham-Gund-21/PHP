a2bcb455d979ba2cd7039238c8ecdb2d83d1d98f
shubham-Gund-21 <angulardeveloper239@gmail.com>
2024-12-20 12:28:42 +0530

    php basic

---
+++ op.php
@@ -0,0 +1,47 @@
+<?php
+// operators
+// 1. Arithmetic operators
+// 2. Comparison operators 
+ // 3. Assignment operators
+ // 4. Logical operators
+ // 5. Bitwise operators
+ // 6. String operators 
+ $x=10;
+ $y=20;
+ echo "Addition: ".$x+$y ."<br>";
+ echo "Subtraction: ".$x+$y ."<br>";
+ echo "division ".$x/$y ."<br>";
+ echo "multiplication " .$x*$y."<br>";
+ echo "modulas".$x%$y ."<br>";
+ $a=5;$b=7;
+//  2. Assignment operators
+ echo "exponentiation " .$a**$b ."<br>";
+ $p=5; $q=3;
+ echo "Addition: ".$p+=$q."<br>";
+ echo "Subtraction: ".$p-=$q."<br>";
+ echo "multiplication: ".$p*=$q."<br>";
+ echo "division: ".$p/=$q."<br>";
+ echo "modulas: ".$p%=$q."<br>";
+ echo "exoponential: ".$p**=$q ."<br>";
+ // 3. Comparison operators
+ $s=5; $p=8;
+ echo "Equal: ".($s==$p)."<br>";
+ echo "strict equality: ".($s===$p)."<br>";
+ echo "Not equal: ".($s!=$p)."<br>";
+ echo "Greater than: ".($s>$p)."<br>";
+ echo "Less than: ".($s<$p)."<br>";
+ echo "Greater than or equal to: ".($s>=$p)."<br>";
+ echo "Less than or equal to: ".($s<=$p)."<br>";
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+?>
